405df1f83c69ca3959ce9b0066047c3fc78304d1
vvmk <vincentmasiello@gmail.com>
2018-04-19 23:05:12 -0400

    Plug.sh puts things where they belong

--- plug.sh
+++ plug.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-
-main() {
-    git clone https://github.com/ctrlpvim/ctrlp.vim ~/.vim/bundle/ctrlp.vim
-    git clone https://github.com/godlygeek/tabular ~/.vim/bundle/tabular
-    git clone https://github.com/tpope/vim-surround ~/.vim/bundle/vim-surround
-    git clone https://github.com/tpope/vim-rails ~/.vim/bundle/vim-rails
-    git clone https://github.com/pangloss/vim-javascript ~/.vim/bundle/vim-javascript
-    git clone https://github.com/tpope/vim-bundler ~/.vim/bundle/vim-bundler
-    git clone https://github.com/hdima/python-syntax ~/.vim/bundle/python-syntax
-    git clone https://github.com/altercation/vim-colors-solarized ~/.vim/bundle/vim-colors-solarized
-}
+echo "Moving subdirectories to ~/.vim/"
+mv ./bundle ~/.vim/bundle
+mv ./colors ~/.vim/colors
+mv ./autoload ~/.vim/colors
+mv ./.vimrc ~/.vimrc
+echo "Downloading plugins..."
+git clone https://github.com/ctrlpvim/ctrlp.vim ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/godlygeek/tabular ~/.vim/bundle/tabular
+git clone https://github.com/tpope/vim-surround ~/.vim/bundle/vim-surround
+git clone https://github.com/tpope/vim-rails ~/.vim/bundle/vim-rails
+git clone https://github.com/pangloss/vim-javascript ~/.vim/bundle/vim-javascript
+git clone https://github.com/tpope/vim-bundler ~/.vim/bundle/vim-bundler
+git clone https://github.com/hdima/python-syntax ~/.vim/bundle/python-syntax
+git clone https://github.com/altercation/vim-colors-solarized ~/.vim/bundle/vim-colors-solarized
+echo ":fire: Done! :fire:"
